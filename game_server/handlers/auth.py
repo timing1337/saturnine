11c9b71a339cbe97322857956c611c37f137e85b
labalityowo <56186498+labalityowo@users.noreply.github.com>
2022-11-28 21:26:55 +0700

    change key algo, time for actual ps dev

--- game_server/handlers/auth.py
+++ game_server/handlers/auth.py
@@ -2,20 +2,25 @@ from game_server.protocol.packet import Packet
 from game_server.protocol.cmd_id import CmdID
 from game_server.encryption import new_key
 from game_server import HandlerRouter,Connection
-from lib.proto import GetPlayerTokenReq,GetPlayerTokenRsp
+from lib.proto import GetPlayerTokenReq,GetPlayerTokenRsp,PlayerLoginReq,PlayerLoginRsp
 import enet
 
 router = HandlerRouter()
 
 @router(CmdID.GetPlayerTokenReq)
 def handle_token_req(conn: Connection, msg: GetPlayerTokenReq):
-    mt_key = new_key(1)
+    mt_key = new_key(int(msg.account_uid))
     rsp = GetPlayerTokenRsp()
     rsp.uid = int(msg.account_uid)
     rsp.account_type = msg.account_type
     rsp.account_uid = msg.account_uid
     rsp.token = msg.account_token
     rsp.gm_uid = int(msg.account_uid)
-    rsp.secret_key_seed = 1
+    rsp.secret_key_seed = int(msg.account_uid)
     conn.send(rsp)
     conn.key = mt_key
+
+def handle_login(conn: Connection, msg: PlayerLoginReq):
+    
+    rsp = PlayerLoginRsp()
+    conn.send(rsp)
