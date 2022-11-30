9db7eda10b53bae890eea86ee78cabc9e0340682
labalityowo <56186498+labalityowo@users.noreply.github.com>
2022-11-30 12:38:04 +0700

    hehe

--- game_server/resource/__init__.py
+++ game_server/resource/__init__.py
@@ -1,7 +1,11 @@
 from game_server.resource.excel import ExcelOutput
 import dataclasses
+from os import path
+from loguru import logger
 
 class ResourceManager:
-    def __init__(self, path: str):
-        self.excels = ExcelOutput.load_all_excels(path)
-        ...
+    def __init__(self, dir: str):
+        if path.exists(dir):
+            self.excels = ExcelOutput.load_all_excels(dir)
+        else:
+            logger.opt(colors=True).debug(f'Resources directory <red>does not exist</red>, running with <yellow>minimal resources</yellow>')
