ae0bb947658c9b33c2bee2666a19159f8da88ea5
Quzalushti <107866393+Quzalushti@users.noreply.github.com>
2022-11-30 22:17:32 +0300

    Teleporting with MarkMapReq

--- saturnine/__main__.py
+++ saturnine/__main__.py
@@ -1,6 +1,6 @@
 from sdk_server.app import run_http_server
 from game_server import GameServer;
-from game_server.handlers import auth, ping, scene, avatar, entity
+from game_server.handlers import auth, ping, scene, avatar, entity, map_tp
 
 if __name__ == "__main__":
     gameserver = GameServer("localhost", 22102)
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     gameserver.add(scene.router)
     gameserver.add(avatar.router)
     gameserver.add(entity.router)
+    gameserver.add(map_tp.router)
 
     gameserver.start()
     run_http_server('0.0.0.0')
